de3b9440ef635a8199d862c6a5b94a97cea48062
chrisrauch193 <chrisrauch193@gmail.com>
2025-03-23 02:10:53 +0900

    Add run scripts checker

--- scripts/00_run_all_scripts.R
+++ scripts/00_run_all_scripts.R
@@ -1,9 +1,16 @@
 # scripts/00_run_all_scripts.R
 
+# Force data retrieve if already existing?
+force <- FALSE
+
 source("scripts/01_install_requirements.R")
 
-source("scripts/02_get_species_metadata.R")
+if (force | !any(grepl("final_anemone_species_list", list.files(".")))  | !any(grepl("final_anemonefish_species_list", list.files(".")))) {
+  source("scripts/02_get_species_metadata.R")
+}
 
-source("scripts/03_download_occurrence_data.R")
+if (force | !any(grepl(".csv", list.files("data/occurrence/anemone")))  | !any(grepl(".csv", list.files("data/occurrence/anemonefish")))) {
+  source("scripts/03_download_occurrence_data.R")
+}
 
 source("scripts/04_download_env_data.R")
