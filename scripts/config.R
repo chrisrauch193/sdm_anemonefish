f8a63afde25794bd9c9aaf1fb477cc8c680c02e4
Chris Rauch <chrisrauch193@gmail.com>
2025-05-15 20:59:19 +0900

    All scripts working properly

--- scripts/config.R
+++ scripts/config.R
@@ -102,7 +102,7 @@ env_scenarios <- c("current", "ssp119_2050", "ssp119_2100", "ssp585_2050", "ssp5
 scenario_folder_map <- list(current = file.path(env_data_dir, "current"), ssp119_2050 = file.path(env_data_dir, "future", "ssp119"), ssp119_2100 = file.path(env_data_dir, "future", "ssp119"), ssp585_2050 = file.path(env_data_dir, "future", "ssp585"), ssp585_2100 = file.path(env_data_dir, "future", "ssp585"))
 terrain_folder <- file.path(env_data_dir, "terrain")
 ssp_scenario_map <- list(ssp119_2050 = "ssp119", ssp119_2100 = "ssp119", ssp585_2050 = "ssp585", ssp585_2100 = "ssp585")
-model_output_subdir_map <- list(`_pca` = "", `_biotic_pc1` = "biotic_pc1", `_combined_pca` = "combined_pca")
+model_output_subdir_map <- list(`_pca` = "", `_biotic_pc4` = "biotic_pc4", `_combined_pca` = "combined_pca")
 
 # Predictor Selection Switch
 use_pca_predictors <- TRUE
@@ -136,14 +136,13 @@ get_display_name <- function(technical_name, lookup = NULL) { if (is.null(lookup
 # --- Spatial Cross-Validation Settings (Simplified blockCV) ---
 # ("spatial_grid" or "spatial_lat" or "random")
 sdm_spatial_cv_type_to_use <- "spatial_grid"
-blockcv_auto_range <- FALSE
-blockcv_range_default <- 20000 # 300000
+blockcv_auto_range <- TRUE
+blockcv_range_default <- 300000 # 20000
 blockcv_range_max <- 1000000
 blockcv_hexagon <- TRUE
 # ("systematic", "random")
 blockcv_selection <- "systematic"
 blockcv_n_iterate <- 300
-
 
 # OBIS stuff
 ecoregion_shapefile <- file.path(shapefile_dir, "MarineRealms_BO.shp")
